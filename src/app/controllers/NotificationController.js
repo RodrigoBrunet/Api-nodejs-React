bb6d66571e756e169cd9848966031c0f300ab3fe
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2020-04-04 10:22:36 -0300

    enviando mensagem agendamento

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,9 @@
+import Notification from '../schemas/notification';
+
+class NotificationController {
+  async index(req, res) {
+    return res.json();
+  }
+}
+
+export default new NotificationController();
